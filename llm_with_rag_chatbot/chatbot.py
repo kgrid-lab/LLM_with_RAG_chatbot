8f5caae3d2af8a1bfeca514404b812d2fcf02ad5
Farid Seifi <farid.seifi@gmail.com>
2024-06-26 16:36:50 -0400

    embedding and vectore store added. qoestion contextualization added. managing history and processing requesdt added.

--- llm_with_rag_chatbot/chatbot.py
+++ llm_with_rag_chatbot/chatbot.py
@@ -38,3 +38,77 @@ for file in files:
     splits.extend(ko)
     
 print(splits)
+
+# Settings for embeddings
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.environ['openai_api_base'], 
+    openai_api_version=os.environ['API_VERSION'],  
+    openai_api_key=os.environ['OPENAI_API_KEY'],   
+    openai_organization=os.environ['OPENAI_organization']   
+)
+print("Embedding documents...")
+# Create the vectorstore
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+print("Documents embedded.")
+
+# Retrieve and generate using the relevant snippets of the data
+retriever = vectorstore.as_retriever(search_kwargs={"k": min(4, len(splits))}) # 4 is default k, ensures we aren't indexing greater than num elts available
+
+# Contextualize question
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    llm, retriever, contextualize_q_prompt
+)
+
+# Answer question
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+Do not simplify the formulas used to calculate the values. \
+If you don't know the answer, just say that you don't know. \
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+# Manage chat history
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+# User requests
+while True:
+  text = input('Enter your query (Example: How many undergrad students are at U of M?): --> ') # Example: How many undergrad students are at U of M?
+  print(conversational_rag_chain.invoke(
+      {"input": text},
+      config={"configurable": {"session_id": "0"}},
+  )["answer"])
