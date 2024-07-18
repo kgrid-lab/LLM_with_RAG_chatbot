77e4907e96b6b8f57f9f77763bf058ea3612e5e3
Farid Seifi <farid.seifi@gmail.com>
2024-07-18 10:14:16 -0400

    api changed to handle background tasks using threading to fix request timeout error in Heroku. The interface is changed to check for the status of the task as well

--- llm_with_rag_chatbot/chatbot.py
+++ llm_with_rag_chatbot/chatbot.py
@@ -1,4 +1,6 @@
 import os
+import threading
+
 from flask import Flask, jsonify, render_template, request
 
 from llm_with_rag_chatbot.openai_chatbot_with_assistant_api import process
@@ -11,6 +13,11 @@ def home():
     return render_template("index.html")
 
 
+def background_task(user_question, chat_history_tuples):
+    bot_response = process(user_question, chat_history_tuples)
+    return bot_response
+
+
 @app.route("/get_response", methods=["POST"])
 def get_response():
     data = request.get_json()
@@ -19,11 +26,20 @@ def get_response():
     chat_history_tuples = [
         (item["question"], item["response"]) for item in chat_history
     ]
-    # Get response from the chatbot
-    bot_response = process(user_question, chat_history_tuples)
+
+    # Start a background thread for the task
+    thread = threading.Thread(
+        target=background_task, args=(user_question, chat_history_tuples)
+    )
+    thread.start()
+    thread.join()  # Wait for the thread to complete
+
+    # Get the result
+    bot_response = background_task(user_question, chat_history_tuples)
+
     return jsonify(response=bot_response)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
