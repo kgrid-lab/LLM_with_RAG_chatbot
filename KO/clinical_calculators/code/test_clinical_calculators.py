43e6be2d4a503180d311e027ff6d885d32a002fa
Niranjan Kumar <niranjak@umich.edu>
2024-12-10 12:13:38 -0500

    Switch test of clinical calculators to use unittest framework

---
+++ KO/clinical_calculators/code/test_clinical_calculators.py
@@ -0,0 +1,25 @@
+import unittest
+
+from cockroft_gault_cr_cl import cockroft_gault_cr_cl
+from ckd_epi_gfr_2021 import ckd_epi_gfr_2021
+from chadsvasc import chadsvasc
+
+class TestClinicalCalculators(unittest.TestCase):
+
+    def test_cockroft_gault_cr_cl(self):
+        self.assertEqual(round(cockroft_gault_cr_cl("female", 40, 70, 0.8)), 103)
+        self.assertEqual(round(cockroft_gault_cr_cl("male", 40, 70, 0.8)), 122)
+
+    def test_ckd_epi_gfr_2021(self):
+        self.assertEqual(round(ckd_epi_gfr_2021("female", 60, 0.8)), 84)
+        self.assertEqual(round(ckd_epi_gfr_2021("male", 60, 0.8)), 101)
+        self.assertEqual(round(ckd_epi_gfr_2021("female", 60, 0.8, 0.75)), 98)
+        self.assertEqual(round(ckd_epi_gfr_2021("male", 60, 0.8, 0.75)), 111)
+
+    def test_chadsvasc(self):
+        self.assertEqual(chadsvasc(62, "female", False, False, False, False, False), 1)
+        self.assertEqual(chadsvasc(68, "female", False, False, False, False, False), 2)
+        self.assertEqual(chadsvasc(68, "male", False, True, False, False, False), 2)
+
+if __name__ == '__main__':
+    unittest.main()
